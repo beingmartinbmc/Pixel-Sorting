628d4c25e36227a16074a42d95448da7df31664c
Ankit Sharma <beingbmc12@gmail.com>
2018-07-17 19:42:07 +0530

    Create Picture.java

---
+++ src/graphics/Picture.java
@@ -0,0 +1,44 @@
+package graphics;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Picture {
+	
+	private int[] pixels;
+	private BufferedImage image;
+	private String path;
+	
+	public Picture(String path) {
+		this.path = path;
+		load();
+	}
+	
+	private void load() {
+		try {
+			image = ImageIO.read(new File(path));
+			int w = image.getWidth();
+			int h = image.getHeight();
+			pixels = new int[w * h];
+			image.getRGB(0, 0, w, h, pixels, 0, w);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int[] getPixels() {
+		return pixels;
+	}
+	
+	public int getWidth() {
+		return image.getWidth();
+	}
+	
+	public int getHeight() {
+		return image.getHeight();
+	}
+
+}
